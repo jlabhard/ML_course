b8833a952aeca8756ef14220ee24afbf6b5b4cc3
jlabhard <jonathan.labhard@gmail.com>
2019-10-17 11:31:56 +0200

    First implementation of least_squares_GD

--- projects/project1/scripts/implementations.py
+++ projects/project1/scripts/implementations.py
@@ -6,9 +6,38 @@ from proj1_helpers import *
 DATA_TRAIN_PATH = '' # TODO: download train data and supply path here
 y, tX, ids = load_csv_data(DATA_TRAIN_PATH)
 
+def compute_loss(y, tx, w):
+    """Calculate the loss.
+
+    You can calculate the loss using mse or mae.
+    """
+    e = y - np.sum(tx * w, axis=1)
+    e2 = e * e
+
+    return np.sum(e2) / (2 * len(y))
+
+def compute_gradient(y, tx, w):
+    """Compute the gradient."""
+    e = y - np.sum(tx*w, axis = 1)
+    return - tx.T.dot(e) / len(y)
+
 def least_squares_GD(y, tx, initial_w, max_iters, gamma) :
-    # TODO: Implementation
-    return NotImplementedError
+    """Gradient descent algorithm."""
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    for n_iter in range(max_iters):
+        gradient = compute_gradient(y, tx, w)
+        loss = compute_loss(y, tx, w)
+        w = w - gamma * gradient
+        # store w and loss
+        ws.append(w)
+        losses.append(loss)
+        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+            bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+
+    return losses, ws
 
 def least_squares_SGD(y, tx, initial_w, max_iters, gamma) :
     # TODO: Implementation
